42fa4a6bfa9056304862e3e29998bb50ee1e5a39
bruvv <3063928+bruvv@users.noreply.github.com>
2025-01-18 11:08:08 +0000

    Update wme-maplinks.js

--- wme-maplinks.js
+++ wme-maplinks.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name             WME-MapLinks
-// @description      Adds links to other maps
-// @namespace        http://tampermonkey.net/
-// @match            *://*.waze.com/*editor*
-// @exclude          *://*.waze.com/user/editor*
-// @grant            none
-// @require          https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.8.0/proj4.js
-// @version          1.3.0
+// @name 			WME-MapLinks
+// @description 	Adds links to other maps
+// @namespace 		http://tampermonkey.net/
+// @author          Robin Breman | L4 Waze NL | @robbre | https://github.com/RobinBreman/WME-MapLinks
+// @match           *://*.waze.com/*editor*
+// @exclude         *://*.waze.com/user/editor*
+// @grant 			none
+// @require         https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.8.0/proj4.js
+// @version 		1.3.0
 // ==/UserScript==
 
 (function() {
